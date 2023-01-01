d8fb4a667412802678c91f042a9b19e361dac691
simonepalmer <simon.e.palmer@outlook.com>
2023-01-01 19:45:43 +0100

    first try at making a program to evaluate a password
    It "works" but I'm not super satisfied with the scoring of the length of the password and will probably try to improve it.
    
    There is also an issue that I haven't fixed yet where if your password is long enough it can get a score higher than 9 and I assume there be issues when that index does not exist in verdict

---
+++ ratePassword.py
@@ -0,0 +1,33 @@
+#Imports & definitions
+import re
+score = 0
+passed = 0
+
+#Entering the password you want rated
+password = input("Enter your password to get it rated!: ")
+print("the password entered is '" + password + "'? ... Let's figure out if it's any good!")
+
+#Scoring your password based on it's contents
+def scoring(checks, password, score, passed):
+    for check in checks:
+        match = re.search(check, password)
+        if match:
+            passed += 1
+            score += 1
+    return score, passed
+
+checks = ['[0-9]', '[A-Z]', '[a-z]', '[$&+,:;=?@#|\'<>.^*()%!-]']
+results = scoring(checks, password, score, passed)
+score, passed = results
+
+#Scoring your password based on it's length
+if passed == 4:
+    score = score + len(password) / 4
+elif passed == 3:
+    score = score + len(password) / 5
+else:
+    score = score + len(password) / 8
+
+#Generating the verdict
+verdict = ['not even a password!', 'really bad!', 'just bad!', 'actually bad!', 'pretty bad!', 'so so.', 'pretty ok!', 'pretty good', 'actually relly good!', 'PERFECT! I love it!']
+print("it's " + verdict[int(score)] + " I'd rate it " + str(int(score)) + "/10!")
