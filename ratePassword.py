be431671a4d518a6cbbf1ef37acb65932f57af17
simonepalmer <simon.e.palmer@outlook.com>
2023-01-01 19:55:24 +0100

    fixed index after letting it rate 0/10
    1. Fixed index after letting it rate 0/10 which as not originally intended
    
    2. Added a quick fix for passwords that are scored higher than the index of verdict

--- ratePassword.py
+++ ratePassword.py
@@ -28,6 +28,10 @@ elif passed == 3:
 else:
     score = score + len(password) / 8
 
+#Keeping the score within verdict index
+if score > 10:
+    score = 10
+
 #Generating the verdict
-verdict = ['not even a password!', 'really bad!', 'just bad!', 'actually bad!', 'pretty bad!', 'so so.', 'pretty ok!', 'pretty good', 'actually relly good!', 'PERFECT! I love it!']
+verdict = ['not even a password!', 'really bad!', 'just bad!', 'actually bad!', 'pretty bad!', 'so so.', 'pretty ok!', 'pretty good', 'actually really good!','a fantastic password!', 'PERFECT! I love it!']
 print("it's " + verdict[int(score)] + " I'd rate it " + str(int(score)) + "/10!")
