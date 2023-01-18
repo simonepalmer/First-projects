b7a3b0c40813629deafa55e40e13beaea3ec8738
simonepalmer <simon.e.palmer@outlook.com>
2023-01-18 07:54:46 +0100

    Limiting range and guesses + misc
    Limiting range and guesses.
    Cleaning up some text outputs.
    Adjusted hint to reflect wider ranges.
    Making it more clear if you won or not in the exit message.

--- guessNumber.py
+++ guessNumber.py
@@ -7,26 +7,40 @@ def welcome_to_the_game():
     print("\nHello, " + name + "!")
     return name # In case I need it for further implementations
 
-def generate_hidden_number():
+def decide_upper_range():
     upper_range = check_type(input("Enter upper range: "))
-    while upper_range <= 20:
-        upper_range = input("\nThat seems a bit too easy, it should probably be over 20! Try again: ")
-        upper_range = check_type(upper_range)
+    while upper_range < 20 or upper_range > 1000:
+        if upper_range < 20:
+            print("\nThat seems a bit too easy, it should probably at least 20!")
+            upper_range = check_type(input("Try again: "))
+        else:
+            print("\nThat seems a bit too high, we dont want to be here all day! Maybe 1000 at most?")
+            upper_range = check_type(input("Try again: "))
     print("\nGood! I'll be thinking of a number between 1 and", upper_range)
-    hidden_number = random.randint(1, upper_range)
-    return hidden_number
+    return upper_range 
 
-def decide_my_max_guesses(hidden_number):
+def decide_my_max_guesses(upper_range):
     my_max_guesses = check_type(input("Enter max number of guesses: "))
-    return my_max_guesses
+    while my_max_guesses > int(upper_range/30+4) or my_max_guesses < int(upper_range/60+2): # +1 to scale a bit on lower ranges
+        if my_max_guesses > int(upper_range/30+4):
+            print("\nThat seems a bit too easy based on the range, it should probably be", int(upper_range/30+4), "at most!")
+            my_max_guesses = check_type(input("Try again: "))
+        else:
+            print("\nThat seems a bit too hard based on the range, it should probably be", int(upper_range/60+2), "at least!")
+            my_max_guesses = check_type(input("Try again: "))
+    print("\nSure, that seems reasonable")
+    return my_max_guesses # There are still some really wonky number guesses on higher ranges :)
+    # I might introduce a appropriate_guesses variable that I can mold into some more reasonable ranges
 
 def print_hint_string(my_guess, hidden_number):        
         guess_dif = hidden_number - my_guess
-        if guess_dif < -20 or guess_dif > 20: # How far off?
-            hint = " way "
+        if guess_dif < -75 or guess_dif > 75: # How far off?
+            hint = " WAY "
+        elif guess_dif < -20 or guess_dif > 20:
+            hint = " much "
         elif guess_dif < -10 or guess_dif > 10:
             hint = " "
-        elif guess_dif < -4 or guess_dif > 4:
+        elif guess_dif < -3 or guess_dif > 3:
             hint = " a little bit "
         elif guess_dif < -1 or guess_dif > 1:
             hint = " just a little bit "
@@ -36,13 +50,13 @@ def print_hint_string(my_guess, hidden_number):
             direction = "lower."
         else:
             direction = "higher."
-        print("\nThat is not correct. My number is", hint, direction, sep="")
+        print("\nThat is not correct. My number is", hint, direction, sep="") # sep="" because of hint=" "
 
 def game_over_or_win(my_max_guesses,my_used_guesses, hidden_number):
     if my_used_guesses >= my_max_guesses:
-        print("\nYou are out of guesses, the correct number was " + str(hidden_number) + ".")
+        print("\nGAME OVER!\nYou are out of guesses, the correct number was " + str(hidden_number) + ".\n")
     else:
-        print("\nThat's right! I was thinking of " + str(hidden_number) + "!")
+        print("\nTHAT IS CORRECT!\nI was indeed thinking of " + str(hidden_number) + "!\n")
 
 def check_type(input_type):
     while type(input_type) != int:
@@ -60,23 +74,27 @@ def check_type(input_type):
 
 def main():
     name = welcome_to_the_game()
+    # Decide difficulty
     print("\nI'm going to think of a number and your are going to guess it. What range do you think is reasonable? 1 to what?")
-    hidden_number = generate_hidden_number()
+    upper_range = decide_upper_range()
+    hidden_number = random.randint(1, upper_range)
     print("\nHow many guesses do you need?")
-    my_max_guesses = decide_my_max_guesses(hidden_number)
+    my_max_guesses = decide_my_max_guesses(upper_range)
+    # Game starts
     my_used_guesses = 0
-    my_guess = input("\nWhat number am I thinking of?: ")
+    my_guess = input("\nLet's start! What number am I thinking of?\nGuess a number: ")
     while str(my_guess) != str(hidden_number):
         my_guess = check_type(my_guess)
         hint_string = print_hint_string(my_guess, hidden_number)
-        my_used_guesses += 1
+        my_used_guesses+=1
         if my_used_guesses == my_max_guesses:
             break
-        if my_used_guesses == my_max_guesses - 1:
+        if my_used_guesses == my_max_guesses-1:
             print("only", my_max_guesses - my_used_guesses, "guess left...")
         else:
             print(my_max_guesses - my_used_guesses, "guesses left")
-        my_guess = input("\nTry again: ")
+        my_guess = input("\nGuess again: ")
+    # Game over
     game_over_or_win(my_max_guesses, my_used_guesses, hidden_number)
 
 if __name__ == "__main__":
